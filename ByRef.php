a8a819d42ae734777d55ce4b6b86caf842373491
imane <bellaghma.imane@gmail.com>
2024-03-17 21:26:55 +0100

    first commit

---
+++ ByRef.php
@@ -0,0 +1,20 @@
+<?php
+
+  /*
+    Assign Variable By Reference
+    - By Default, Variables Are Always Assigned By Value
+    - Assigned By Reference Make Variable Alias Or Point To Another
+
+    Search
+    - References Are Not Pointers
+  */
+
+  $a = "Osama";
+ // $b = &#038;$a;
+  $b = "Elzero";
+  $a = "School";
+
+  echo $a;//school
+  echo '<br>';
+  echo $b;//school
+  ?>
---
+++ cours17Predefined.php
@@ -0,0 +1,23 @@
+<?php
+
+  /*
+    Pre-Defined Variables
+
+    Search
+    - PHP Pre-Defined Variables
+  */
+
+  // echo '<pre>';
+  // print_r($_SERVER);
+  // echo $_SERVER["HTTP_CONNECTION"];
+  // echo '</pre>';
+
+  echo $_GET["username"];
+//$_POST
+//$_get ytb3 l9ima lfog
+?>
+
+<form action="" method="get">
+  <input type="text" name="username">
+  <input type="submit" value="Send">
+</form>
